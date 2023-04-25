6cd6fa6e3e8a34e696da5dc42c120a3adceb7326
Rob Winch <rwinch@users.noreply.github.com>
2023-04-25 15:16:22 -0500

    try to trim ids

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import com.google.gson.Gson;
 
 import java.net.URI;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DemoApplication {
@@ -31,18 +32,28 @@ public class DemoApplication {
 			}
 		});
 		Map<String, String> asciidoctorIdToUrl = new HashMap<>();
+		Map<String, String> asciidoctorIdToTrimmedId = new HashMap<>();
 		IdParser asciidoctorIdParser = IdParser.createForAsciidoctor();
 		String asciidoctorStartUrl = "https://docs.spring.io/spring-framework/docs/current/reference/html/";
 		webCrawler.crawl(asciidoctorStartUrl, page -> {
 			String url = page.getUrl().toString();
-			for (String id : asciidoctorIdParser.ids(page)) {
+			List<String> ids = asciidoctorIdParser.ids(page);
+			String h1Id = ids.isEmpty() ? "" : ids.get(0);
+			IdTrimmer trimmer = new IdTrimmer(h1Id);
+			for (String id : ids) {
+				String trimmedId = trimmer.trim(id);
 				asciidoctorIdToUrl.put(id, url);
+				asciidoctorIdToTrimmedId.put(id, trimmedId);
 			}
 		});
 		for (String asciidoctorId : asciidoctorIdToUrl.keySet()) {
 			if (!antoraIdToUrl.containsKey(asciidoctorId)) {
 				if (antoraOriginalIdToUrl.containsKey(asciidoctorId)) {
-					System.out.println("Couldn't find mapping for " + asciidoctorId);
+					String trimmedId = asciidoctorIdToTrimmedId.get(asciidoctorId);
+					if (!(antoraOriginalIdToUrl.containsKey(trimmedId) || antoraIdToUrl.containsKey(trimmedId))) {
+
+						System.out.println("Couldn't find mapping for " + asciidoctorId);
+					}
 				}
 			}
 		}
