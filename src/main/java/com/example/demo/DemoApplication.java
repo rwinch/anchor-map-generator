c7a314004bfe59f3674954cdd060960096873f88
Rob Winch <rwinch@users.noreply.github.com>
2023-04-25 13:54:50 -0500

    Try and guess asciidoctor id based on path

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.google.gson.Gson;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,15 +14,31 @@ public class DemoApplication {
 	public static void main(String[] args) throws Exception {
 		Map<String, String> antoraIdToUrl = new HashMap<>();
 		IdParser antoraIdParser = IdParser.createForAntora();
-		String antoraStartUrl = "https://docs.spring.io/spring-security/reference/";
+		String antoraStartUrl = "https://rwinch.github.io/spring-framework/";
 		WebCrawler webCrawler = new WebCrawler();
 		webCrawler.crawl(antoraStartUrl, page -> {
 			String url = page.getUrl().toString();
-			System.out.println(url);
+			int lastIndexOfPath = url.lastIndexOf('/');
+			String basePath = (lastIndexOfPath > antoraStartUrl.length()) ? url.substring(antoraStartUrl.length(), lastIndexOfPath) : "";
+			String baseId = basePath.replaceAll("/", ".") + ".";
 			for (String id : antoraIdParser.ids(page)) {
-				antoraIdToUrl.put(id, url);
+				antoraIdToUrl.put(baseId + id, url +"#"+id);
 			}
 		});
+		Map<String, String> asciidoctorIdToUrl = new HashMap<>();
+		IdParser asciidoctorIdParser = IdParser.createForAsciidoctor();
+		String asciidoctorStartUrl = "https://docs.spring.io/spring-framework/docs/current/reference/html/";
+		webCrawler.crawl(asciidoctorStartUrl, page -> {
+			String url = page.getUrl().toString();
+			for (String id : asciidoctorIdParser.ids(page)) {
+				asciidoctorIdToUrl.put(id, url);
+			}
+		});
+		for (String asciidoctorId : asciidoctorIdToUrl.keySet()) {
+			if (!antoraIdToUrl.containsKey(asciidoctorId)) {
+				System.out.println("Couldn't find mapping for " + asciidoctorId);
+			}
+		}
 		Gson gson = new Gson();
 		String json = gson.toJson(antoraIdToUrl);
 		System.out.println(json);
