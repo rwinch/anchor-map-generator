d23a45485669ed17d18ea65aeb7854200bcdd45b
Rob Winch <rwinch@users.noreply.github.com>
2023-04-25 13:35:37 -0500

    IdParser for antora and asciidoctor

--- src/main/java/com/example/demo/IdParser.java
+++ src/main/java/com/example/demo/IdParser.java
@@ -9,12 +9,29 @@ import java.util.List;
 import java.util.Set;
 
 public class IdParser {
+	private final String idXpath;
+
+	private IdParser(String idXpath) {
+		this.idXpath = idXpath;
+	}
+
 	public List<String> ids(HtmlPage page) {
 		List<String> ids = new ArrayList<>();
-		List<HtmlElement> elementsWithId = page.getByXPath("//article//*[@id][@id!=\"\"]");
+		List<HtmlElement> elementsWithId = page.getByXPath(this.idXpath);
 		for (HtmlElement element : elementsWithId) {
-			ids.add(element.getId());
+			String id = element.getId();
+			if (!Set.of("page-title", "preamble").contains(id) && !id.startsWith("_tabs_")) {
+				ids.add(element.getId());
+			}
 		}
 		return ids;
+	}
+
+	public static IdParser createForAntora() {
+		return new IdParser("//article//*[@id][@id!=\"\"]");
+	}
+
+	public static IdParser createForAsciidoctor() {
+		return new IdParser("//div[@id=\"content\"]//*[@id]");
 	}
 }
